6003b00bd2bfdb7c30ceb44979e5a7c843f035be
oubes <omargamal.bs99@gmail.com>
2026-04-26 16:22:50 +0300

    Refactor settings.py to improve organization by adding section headers and restructuring environment variable handling

--- config/settings.py
+++ config/settings.py
@@ -1,12 +1,19 @@
 # ---- Imports ---- #
+from pathlib import Path
 import os
 from dotenv import load_dotenv
 
 # ---- Load Environment Variables ---- #
 load_dotenv()
 
-FILE_PATH: str = os.getenv("FILE_PATH") # type: ignore
+# ---- Base Directory (project root) ---- #
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-#  ---- Validation ----
-if not FILE_PATH:
+# ---- Env Variable ---- #
+_raw_file_path = os.getenv("FILE_PATH")
+
+if not _raw_file_path:
     raise ValueError("FILE_PATH is not set in .env")
+
+# ---- Resolve Path ---- #
+FILE_PATH: Path = (BASE_DIR / _raw_file_path).resolve()
