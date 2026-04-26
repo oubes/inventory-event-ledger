f95ab24ec72ac84e1821332dc4b7ad518be1dac8
oubes <omargamal.bs99@gmail.com>
2026-04-26 15:54:21 +0300

    Refactor settings.py to improve organization by adding section headers for imports and environment variable loading

--- config/settings.py
+++ config/settings.py
@@ -1,9 +1,12 @@
+# ---- Imports ---- #
 import os
 from dotenv import load_dotenv
 
+# ---- Load Environment Variables ---- #
 load_dotenv()
 
 FILE_PATH: str = os.getenv("FILE_PATH") # type: ignore
 
+#  ---- Validation ----
 if not FILE_PATH:
     raise ValueError("FILE_PATH is not set in .env")
