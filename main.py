579e2be26e71d0bf4771ecc68987df81da6ae004
oubes <omargamal.bs99@gmail.com>
2026-04-26 14:44:29 +0300

    Refactor main function to replace export_item_tables with specific export functions for better clarity and organization

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ from config.settings import FILE_PATH
 from ingestion.reader import read_log
 from parsing.parser import split_log_line
 from processing.transform import to_dataframe
-from output.csv_exporter import export_item_tables
+from output.csv_exporter import (
+    export_aggregated_by_time,
+    export_aggregated_by_selling,
+    export_sales_trend,
+    export_product_contribution,
+    export_detect_sales_periods,
+    export_sales_stability
+)
 
 
 def main():
@@ -17,8 +24,25 @@ def main():
     df = to_dataframe(data)
 
     # OUTPUT LAYER
-    export_item_tables(df, "outputs\\days", freq="D") # Daily frequency
-    export_item_tables(df, "outputs\\months", freq="ME") # Monthly frequency
+    # aggregated time
+    export_aggregated_by_time(df, "outputs/aggregated_time/days", "D")
+    export_aggregated_by_time(df, "outputs/aggregated_time/months", "ME")
+
+    # aggregated selling
+    export_aggregated_by_selling(df, "outputs/aggregated_selling/days", "D")
+    export_aggregated_by_selling(df, "outputs/aggregated_selling/months", "ME")
+
+    # trend
+    export_sales_trend(df, "outputs/trend/days", "D")
+    export_sales_trend(df, "outputs/trend/months", "ME")
+
+    # periods
+    export_detect_sales_periods(df, "outputs/periods/days", "D")
+    export_detect_sales_periods(df, "outputs/periods/months", "ME")
+
+    # overall (no freq)
+    export_product_contribution(df, "outputs/contribution/overall")
+    export_sales_stability(df, "outputs/stability/overall")
 
 if __name__ == "__main__":
     main()
